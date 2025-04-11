f3241f8a3b98d3a8e588fe0a306294e15bf684e7
marvin-skyparking <marvin.johannes@skyparking.co.id>
2025-04-11 15:25:49 +0700

    fixing_parsedqs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,19 +1,22 @@
+import { Router } from 'express';
 import express from 'express';
-import partnerRoute from './partner.routes';
-import lotRoute from './lot_management.routes';
-import innAppRoute from './inapp.routes';
-import healthRoute from './connection_health.routes';
-
-const router = express.Router();
 
 // router.use('/customer', member_customer);
 // router.use('/auth', authRouter);
 // router.use('/location', locationrouter);
 // router.use('/product', productRouter);
 // router.use('/productPurchase', purchaseProduct);
+import partnerRoute from './partner.routes';
+import lotRoute from './lot_management.routes';
+import innAppRoute from './inapp.routes';
+import healthRoute from './connection_health.routes';
+
+const router: Router = express.Router();
+
 router.use('/connection', healthRoute);
 router.use('/partner', partnerRoute);
 router.use('/realtime', lotRoute);
 router.use('/parking', innAppRoute);
 
-export default router;
+const typedRouter: Router = router;
+export default typedRouter;
