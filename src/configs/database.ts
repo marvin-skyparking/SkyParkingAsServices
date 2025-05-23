885696c681342792582f0385ccf98a08bb777683
marvin-skyparking <marvin.johannes@skyparking.co.id>
2025-05-23 11:17:13 +0700

    add_config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,24 +1,26 @@
 import { Sequelize } from 'sequelize';
 import EnvConfig from './env.config';
 
+const isProd = EnvConfig.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(
-  EnvConfig.DB_DEV,
-  EnvConfig.USERNAME_DB_DEV,
-  EnvConfig.PASSWORD_DB_DEV,
+  isProd ? EnvConfig.DB_NAME_PROD : EnvConfig.DB_NAME_DEV,
+  isProd ? EnvConfig.USERNAME_DB_PROD : EnvConfig.USERNAME_DB_DEV,
+  isProd ? EnvConfig.PASSWORD_DB_PROD : EnvConfig.PASSWORD_DB_DEV,
   {
-    host: '8.215.44.147',
+    host: isProd ? EnvConfig.HOST_DB_PROD : EnvConfig.HOST_DB_DEV,
     dialect: 'mariadb',
     logging: false,
     dialectOptions: {
-      connectTimeout: 60000, // 60 seconds
-      timezone: '+07:00' // Set timezone to UTC+7
+      connectTimeout: 100000
     },
     pool: {
-      max: 500,
-      min: 20,
-      acquire: 60000,
-      idle: 20000
-    }
+      max: 50, // allow up to 50 concurrent DB connections
+      min: 10, // keep at least 10 alive for quick reuse
+      acquire: 60000, // wait up to 60 seconds for a connection
+      idle: 10000 // release connection if idle for 10 seconds
+    },
+    timezone: '+07:00'
   }
 );
 
--- src/interfaces/env.interface.ts
+++ src/interfaces/env.interface.ts
@@ -2,8 +2,13 @@ export interface IEnvInterface {
   NODE_ENV: string;
   APP_URL: string;
   PORT: string;
-  DB_DEV: string;
+  HOST_DB_PROD: string;
+  HOST_DB_DEV: string;
+  DB_NAME_DEV: string;
+  DB_NAME_PROD: string;
   USERNAME_DB_DEV: string;
+  USERNAME_DB_PROD: string;
+  PASSWORD_DB_PROD: string;
   PASSWORD_DB_DEV: string;
   ENABLE_SWAGGER: boolean;
   SENTRY_URI: string;
