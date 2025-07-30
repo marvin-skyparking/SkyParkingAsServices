39acfc67d6db95b414d64c36fc0b134827c27ab1
marvin-skyparking <marvin.johannes@skyparking.co.id>
2025-07-30 13:27:54 +0700

    deploy simulator

--- src/routes/inapp.routes.ts
+++ src/routes/inapp.routes.ts
@@ -2,17 +2,17 @@ import express from 'express';
 import timeout from 'connect-timeout';
 import {
   // close_ticket,
-  // close_ticket_not_encrypt,
-  Inquiry_Transaction,
-  Inquiry_Transaction_Snap,
+  close_ticket_not_encrypt,
+  // Inquiry_Transaction,
+  // Inquiry_Transaction_Snap,
   // InquiryTransactionSnap,
-  Payment_Confirmation,
-  Payment_Confirmation_QRIS
-  // processInquiryTransaction,
-  // processInquiryTransactionEncrypt,
-  // processPaymentTransaction,
-  // processPaymentTransactionEncrypt,
-  // processPaymentTransactionPOST
+  // Payment_Confirmation,
+  // Payment_Confirmation_QRIS
+  processInquiryTransaction,
+  processInquiryTransactionEncrypt,
+  processPaymentTransaction,
+  processPaymentTransactionEncrypt,
+  processPaymentTransactionPOST
 } from '../controllers/transaction.controller';
 import {
   createTicketHandler,
@@ -38,54 +38,54 @@ const innAppRoute = express.Router();
 // innAppRoute.post('/Partner/CloseTicket', haltOnTimeout, close_ticket);
 
 // POST TEST
-innAppRoute.post(
-  '/Partner/InquiryTariffREG',
-  haltOnTimeout,
-  Inquiry_Transaction
-);
-innAppRoute.post(
-  '/Partner/PaymentConfirmationREG',
-  haltOnTimeout,
-  Payment_Confirmation
-);
+// innAppRoute.post(
+//   '/Partner/InquiryTariffREG',
+//   haltOnTimeout,
+//   Inquiry_Transaction
+// );
+// innAppRoute.post(
+//   '/Partner/PaymentConfirmationREG',
+//   haltOnTimeout,
+//   Payment_Confirmation
+// );
 
-innAppRoute.post('/Membership/StylesCheckMembership', auto_entry);
+// innAppRoute.post('/Membership/StylesCheckMembership', auto_entry);
 
-innAppRoute.post(
-  '/Partner/PaymentConfirmationQRIS',
-  haltOnTimeout,
-  Payment_Confirmation_QRIS
-);
+// innAppRoute.post(
+//   '/Partner/PaymentConfirmationQRIS',
+//   haltOnTimeout,
+//   Payment_Confirmation_QRIS
+// );
 
 //Simulator
 innAppRoute.post('/Signature-Inquiry', sigantureKey);
 innAppRoute.post('/Signature-Payment', getPaymentSignature);
 innAppRoute.post('/GenerateTicket', createTicketHandler);
-// innAppRoute.post(
-//   '/POST/Simulator/InquiryTariffREG/',
-//   processInquiryTransaction
-// );
-// innAppRoute.post(
-//   '/Partner/Simulator/InquiryTariffREG/',
-//   processInquiryTransaction
-// );
-// innAppRoute.post(
-//   '/Partner/Simulator/PaymentConfrimationREG/',
-//   processPaymentTransaction
-// );
-// innAppRoute.post(
-//   '/POST/Simulator/PaymentConfrimationREG/',
-//   processPaymentTransactionPOST
-// );
+innAppRoute.post(
+  '/POST/Simulator/InquiryTariffREG/',
+  processInquiryTransaction
+);
+innAppRoute.post(
+  '/Partner/Simulator/InquiryTariffREG/',
+  processInquiryTransaction
+);
+innAppRoute.post(
+  '/Partner/Simulator/PaymentConfrimationREG/',
+  processPaymentTransaction
+);
+innAppRoute.post(
+  '/POST/Simulator/PaymentConfrimationREG/',
+  processPaymentTransactionPOST
+);
 
-// innAppRoute.post('/POST/Simulator/Close-Ticket/', close_ticket_not_encrypt);
+innAppRoute.post('/POST/Simulator/Close-Ticket/', close_ticket_not_encrypt);
 
 //Version 2
-innAppRoute.post(
-  '/Partner/ticket/InquiryTariffREG',
-  verifyClientAuthAccess,
-  Inquiry_Transaction_Snap
-);
+// innAppRoute.post(
+//   '/Partner/ticket/InquiryTariffREG',
+//   verifyClientAuthAccess,
+//   Inquiry_Transaction_Snap
+// );
 // innAppRoute.post('/Partner/PaymentConfrimationREG', processPaymentTransaction);
 
 export default innAppRoute;
