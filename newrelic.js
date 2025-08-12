264f8bac492c5153e5b8564724b6003c771c214c
marvin-skyparking <marvin.johannes@skyparking.co.id>
2025-08-12 13:25:27 +0700

    add_new_relic

---
+++ newrelic.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ */
+exports.config = {
+  app_name: ['Your App Name'],
+  license_key: 'YOUR_NEW_RELIC_LICENSE_KEY',
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
