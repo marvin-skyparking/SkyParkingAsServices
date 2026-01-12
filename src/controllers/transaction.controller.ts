418e11c25a365f1c235cb219fb851c8a5f51be44
Marvin Johannes <marvin.johannes@skyparking.co.id>
2026-01-12 09:45:47 +0700

    add voucher usage

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -5136,3 +5136,96 @@ export async function SEND_VALET_NUMBER_VERIFICATION(
     });
   }
 }
+
+// export async function SendVoucherToLMI(
+//   req: Request,
+//   res: Response
+// ): Promise<any> {
+//   try {
+//     const {
+//       login,
+//       password,
+//       merchantID,
+//       locationCode,
+//       transactionNo,
+//       licensePlateNo,
+//       inTime,
+//       gateInCode,
+//       vehicleType,
+//       totalTariff,
+//       outTime,
+//       gateOutCode
+//     } = req.body;
+
+//     const secretKey = 'SECRET_KEY';
+//     const partner_key = 'PARTNER_KEY';
+
+//     // 1. Generate signature
+//     const remoteSignature = generateSignatureLMI(
+//       login,
+//       password,
+//       merchantID,
+//       locationCode,
+//       transactionNo,
+//       licensePlateNo,
+//       inTime,
+//       gateInCode,
+//       vehicleType,
+//       totalTariff,
+//       outTime,
+//       gateOutCode,
+//       secretKey
+//     );
+
+//     // 2. Combine data + signature
+//     const data = {
+//       ...req.body,
+//       signature: remoteSignature
+//     };
+
+//     //Find Location
+//     const location = await findLocationStoreCodeData(locationCode);
+
+//     if (!location) {
+//       return res.status(404).json({
+//         message: 'Location Invalid'
+//       });
+//     }
+
+//     // 3. Encrypt and send
+//     const encryptdata = EncryptTotPOST(data, partner_key);
+
+//     const send_to_LMI = await axios.post(EnvConfig.URL_VOUCHER_USAGE_LMI, {
+//       data: encryptdata
+//     });
+
+//     console.log(send_to_LMI.data.data);
+//     // 4. Decrypt response
+//     const decrypt_response = DecryptTotPOST(send_to_LMI.data.data, partner_key);
+
+//     // 5. Insert into database
+//     await createVoucherUsage({
+//       CompanyName: location.CompanyName, // optional: adjust as needed
+//       MerchantID: merchantID,
+//       LocationCode: locationCode,
+//       TransactionNo: transactionNo,
+//       LicensePlateNo: licensePlateNo,
+//       InTime: new Date(inTime),
+//       GateInCode: gateInCode,
+//       VehicleType: vehicleType,
+//       TotalTariff: totalTariff,
+//       OutTime: new Date(outTime),
+//       GateOutCode: gateOutCode,
+//       MerchantDataRequest: JSON.stringify(data),
+//       MerchantDataResponse: JSON.stringify(decrypt_response),
+//       CreatedBy: login,
+//       CreatedOn: new Date()
+//     });
+
+//     // 6. Return API response
+//     return res.status(200).json(decrypt_response);
+//   } catch (err) {
+//     console.error('Voucher usage error:', err);
+//     return res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// }
