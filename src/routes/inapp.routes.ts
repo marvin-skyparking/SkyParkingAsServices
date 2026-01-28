efd33cc3c4f2da1ee370a24901fc4f4010ebbae3
Marvin Johannes <marvin.johannes@skyparking.co.id>
2026-01-28 15:57:24 +0700

    update voucher route

--- src/routes/inapp.routes.ts
+++ src/routes/inapp.routes.ts
@@ -96,11 +96,11 @@ innAppRoute.post('/Partner/EncryptSimulator', encryptPayloadSimulator);
 
 // innAppRoute.post('/POST/Simulator/Close-Ticket/', close_ticket_not_encrypt);
 
-//Version 2 - CANCELED
-innAppRoute.post('/Partner/B2B/Token', B2B_TOKEN_IN_APP);
+// //Version 2 - CANCELED
+// innAppRoute.post('/Partner/B2B/Token', B2B_TOKEN_IN_APP);
 
-innAppRoute.post('/Partner/ticket/InquiryTarif', Inquiry_Transaction_Snap);
-// innAppRoute.post('/Partner/PaymentConfrimationREG', processPaymentTransaction);
+// innAppRoute.post('/Partner/ticket/InquiryTarif', Inquiry_Transaction_Snap);
+// // innAppRoute.post('/Partner/PaymentConfrimationREG', processPaymentTransaction);
 
 //VALET TO POST
 innAppRoute.post('/Partner/ValetVerify', SEND_VALET_NUMBER_VERIFICATION);
